c7a35ad7b43e5cfbb5b7c6ebccb643100a98a6ed
Meg MacIver <megmaciver13@gmail.com>
2017-12-18 16:39:56 -0500

    rendered front end!!

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react'
-import SubjectBox from '../SubjectBox/SubjectBox'
+import axios from 'axios'
 
+import SubjectBox from '../SubjectBox/SubjectBox'
 import './Home.css'
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Redirect,
-  Switch
-} from 'react-router-dom'
 
 class Home extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props)
+
     this.state = {
-       subjects: []
-     }
-   }
+      subjects: []
+    }
+  }
+
+  componentDidMount () {
+    axios.get('http://localhost:3001/subjects')
+      .then(subjects => {
+        console.log(subjects.data)
+        this.setState({
+          subjects: subjects.data
+        })
+      })
+      .catch(err => console.log(err))
+  }
 
   render () {
     let subjectBoxes = this.state.subjects.map((subject, i) => {
       return <SubjectBox info={subject} key={i} />
     })
     return (
-      <div>
-        <div className="subjects">
-          {subjectBoxes}
-        </div>
+      <div className="subjects">
+        {subjectBoxes}
       </div>
     )
   }
--- src/SubjectBox/SubjectBox.js
+++ src/SubjectBox/SubjectBox.js
@@ -1,29 +1,24 @@
 import React, {Component} from 'react'
+import { Link } from 'react-router-dom'
+
 import './SubjectBox.css'
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Redirect,
-  Switch
-} from 'react-router-dom'
 
 class subject extends Component {
-  constructor(props){
+  constructor (props) {
     super(props)
     this.state = {
-      name: this.props.subject.name,
-      subjectImage: this.props.subject.subjectImage,
-      id: this.props.subject._id
+      name: this.props.info.name,
+      subjectImage: this.props.info.subjectImage,
+      id: this.props.info._id
     }
   }
-  render() {
-    let {name, subjectImage, lessons} = this.state
-    return(
-      <div name="subject-container">
-        <Link to={`/subjects/${this.state.id}`}>
-          <img src={subjectImage} alt={name}/>
-          <h2 subName="subject-name">{name}</h2>
+
+  render () {
+    return (
+      <div className="subject-container">
+        <Link to={`/subjects/${this.state._id}`}>
+          <img src={this.state.subjectImage} alt={this.state.name} />
+          <h2 className="subject-name">{this.state.name}</h2>
         </Link>
       </div>
     )
