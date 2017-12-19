201f39479c8365600c906df0e90ddbb092a4610f
Stella <sgordonzigel@gmail.com>
2017-12-19 11:19:11 -0500

    fix lesson and question component - NEEDS TESTING

--- src/components/Lesson/Lesson.js
+++ src/components/Lesson/Lesson.js
@@ -5,11 +5,30 @@ import './Lesson.css'
 class Lesson extends Component {
   constructor (props) {
     super(props)
+
+    this.state = {
+      lesson: {}
+    }
+  }
+
+  componentDidMount() {
+    axios
+      .get(`http://localhost:3001/subjects/${this.props.match.params.subject_id}/lesson/${this.props.match.params._id}`)
+      .then(lesson =>
+        this.setState({
+          lesson: lesson.data
+        })
+      )
+      .catch(err => console.log(err))
   }
 
   render () {
+    let questions = this.state.lesson.questions.map((question, i) +. {
+      return <Question info={question} key={i} />
+    })
     return (
-      <div>
+      <div className="questions">
+        {questions}
         <p>Hello World</p>
       </div>
     )
