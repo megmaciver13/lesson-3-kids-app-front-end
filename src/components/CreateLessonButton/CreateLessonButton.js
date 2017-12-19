e0fb658a4dabbf0cfbd21bf2ff3a16bd4105fa24
Wilner Charles <wilnercharles3@gmail.com>
2017-12-19 11:12:13 -0500

    CreateLessonButton

---
+++ src/components/CreateLessonButton/CreateLessonButton.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import CreateLessonButton from './createLessonButton';
+
+class CreateLesson extends Component {
+  render() {
+    return <Button color="blue" />;
+  }
+}
+
+export default CreateLesson;
