e2f63343902991999edbe67327fec9bcbfe83ef8
Stella <sgordonzigel@gmail.com>
2017-12-20 11:21:07 -0500

    fix lesson.js and questionbox.js and start newlesson.js but this is not finished

---
+++ src/components/NewLesson/NewLesson.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react"
+import axios from "axios"
+import { Link, Redirect } from "react-router-dom"
+
+class NewLesson extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      hasSubmitted: false,
+      name: null,
+      lessonImage: null
+    }
+    this.handleName = this.handleName.bind(this)
+    this.handleLessonImage = this.handleLessonImage.bind(this)
+  }
+
+  handleName(e) {
+    this.setState({
+      name: e.target.value
+    })
+  }
+
+  handleLessonImage(e) {
+    this.setState({
+      lessonImage: e.target.value
+    })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log(this.state)
+    this.setState({
+      hasSubmitted: true,
+    })
+    axios
+      .post('http://localhost:3001/', {
+        name: this.state.name,
+        lessonImage: this.state.lessonImage
+      })
+      .then((response) => {
+        console.log(response)
+        this.props.history.push('/subjects/:subject_id')
+      })
+      .catch(err => console.log(err))
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <input
+            type="text"
+            placeholder="Lesson Name"
+            onChange={this.handleName}
+          />
+          <input
+            type="text"
+            placeholder="Icon URL"
+            onChange{this.handleLessonImage}
+          />
+          <input type="submit" value="submit" />
+        </form>    
+      </div>
+    )
+  }
+
+
+}
+
+export default NewLesson
